4a2231631e30645e7c3af15b54b1949a11b7ae20
vienpham2019 <vienpham2019@gmail.com>
2021-01-12 21:51:56 -0600

    change channel bind room

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,52 +1,44 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import Pusher from "pusher-js";
-import {
-  A_set_rooms,
-  A_set_current_room,
-  A_set_current_room_id,
-} from "./reducer/rootAction";
+import { A_set_rooms } from "./reducer/rootAction";
 
 import Rooms from "./components/Rooms";
 import Messages from "./components/Messages";
 
 export default function App() {
   const dispatch = useDispatch();
-  const { rooms, current_room } = useSelector((state) => state);
   useEffect(() => {
     fetch("http://localhost:3000/rooms")
       .then((res) => res.json())
       .then((data) => {
         let rooms = new Map();
-        for (let room of data.rooms) {
-          rooms.set(room.id, { comments: room.comments, title: room.title });
+        for (let { id, comments, title } of data.rooms) {
+          rooms.set(id, { comments, title });
         }
         dispatch(A_set_rooms(rooms));
-        dispatch(A_set_current_room_id(1));
 
-        var pusher = new Pusher("391a91223a701823767a", {
+        let pusher = new Pusher("391a91223a701823767a", {
           cluster: "us2",
         });
 
         let channel = pusher.subscribe("my-channel");
 
-        const bind_room = ({ id }) => {
-          channel.bind(`room_${id}`, function (_data) {
-            let { message } = _data.messages;
-            rooms.get(id).comments.push({ message });
-            dispatch(A_set_rooms(rooms));
-          });
-        };
-
-        for (let room of data.rooms) {
-          bind_room(room);
-        }
+        channel.bind("send_message", function (_data) {
+          let { messages, room_id } = _data;
+          let { message } = messages;
+          rooms.get(room_id).comments.push({ message });
+          dispatch(A_set_rooms(rooms));
+        });
 
         channel.bind("new_room", ({ room }) => {
           let { id, comments, title } = room;
           rooms.set(id, { comments, title });
           dispatch(A_set_rooms(rooms));
-          bind_room(room);
+        });
+
+        channel.bind("user_typing_event", (_data) => {
+          console.log(_data);
         });
       });
   }, []);
